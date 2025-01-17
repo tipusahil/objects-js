fa2fa211d7ce908732968d2c95657c173997ae56
tipusahil <tipusahil.ctg@gmailcom>
2025-01-17 19:16:08 +0600

    3rd commit

--- obj_change_system.js
+++ obj_change_system.js
@@ -5,32 +5,29 @@ const person = {
     agee : 22,
     skill: 'web magicial',
   "amar book": "english",
+
 }
 
 console.log(person);//output: person object er sob property er value output e dekabe.karon direct object name tai print kora hoise.
 
+console.log(person.agee);//output : (person) obj er (agee) property er value (22) dekabe.
 
-// jodi kuno 
-console.log(person.agee);/*output : (person) obj er (agee) property er value (22) dekabe.
+const boyos = person.agee;// obj er key er value evabe ekta variable er modde raka jai. & caile seta print korew deka jai.jmn: 
+console.log(boyos);// output: 22
 
-22 , evabei multo object er property/key er value deka hoi. print kora hoi 
 
-abr jodi obj er property/key er value change kortee cai thle 2ta niome change kora jabe, jmn:
 
-1. dot(.)diye jmn: person.agee ="notun value";
-2.bracket diye jmn: person["agee"] = "notun value" 
-bracket diye change koratai subida karon jodi property er name multiple word hoi tkn dot(.) diye value change korte kinba value dektte moskil,kintut brackett diye hole obj er property/key name jodi multiple word o hoi thlew change kora kinba write kora jai.jmn: 
 
-person.amr book = 'english'; //but eta error dibe . diye multiple wor wala property lika jaina.
+//...............obj er property er value change, pirnt korar 2 ways................
 
-person['amar book'] = 'english';  eta tik ase.
+// obj er property/key er value change kinba print korte caile 2ta way te kora jai 1.dot(.)notation diye jmn: 
+//person.amr book = 'english';// but eta error dibe . diye multiple wor wala property lika jaina.
 
-*/
 
-/*object er kuno property/key er value change
-object er name(person) then dot (.) erpore jei property er value change korte cai sei property er name then =  sign er pore jei notun value ditee cai ota dite hobe
+// otoba 2.(['']) 3rd bracket notation diye, etai recomended ,,3rd bracket er vitore qutetion er viotre property name diye jmn: 
+person['amar book'] = 'ICT';//  eta tik ase.
+console.log(person['amar book']);// output:  ICT.
 
-*/
-console.log(person);// output e 
+console.log(person);// output: person obj er changed kora data soho all data dekabe.
 
 // node obj_change_system.js
