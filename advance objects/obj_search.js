9b7929f747bee1fb12d4473423b42e4d4a8507c7
tipusahil <tipusahil.ctg@gmail.com>
2025-02-08 10:52:05 +0600

    advance object

---
+++ advance objects/obj_search.js
@@ -0,0 +1,8 @@
+const products =[ 
+ {name:'apple',         brand:'apple',      price:30000,type:'phone'},
+ {name:'samsung',       brand:'samsung',    price:32000,type:'phone'},
+ {name:'hp laptop',     brand:'microsoft',  price:33000,type:'laptop'},
+ {name:'macbook laptop',brand:'apple',      price:133000,type:'laptop'},
+ {name:'realme',        brand:'xiomi',      price:10000,type:'phone'},
+];
+
